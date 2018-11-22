3a34ee61c3ab8268b8c43486d1817a7e5b17c3ae
cihapus <cihapus@gmail.com>
2018-11-22 20:22:44 +0000

    Update cb.js

--- cb.js
+++ cb.js
@@ -8,12 +8,27 @@ console.log("sort files called....");
   
   if(studentfiles){
   console.log(studentfiles);
-  }
+    
+    var geteachfile = student.querySelectorAll("tr");
+    
+    if(geteachfile.length>0){
+    
+      for(let i=0;i<geteachfile.length;i++){
+        
+        console.log(geteachfile[i]);
+      
+      }//forloop
+      
+      
+    }//if geteachfile
+    
+    
+  }//if studentfiles
   else{
   console.log("failed to find report results");
   }
 
 
-}
+}//sortfiles
 
 sortFiles();
