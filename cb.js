c579e780dbca95929e4d48ac811b73c3bd9cef8f
cihapus <cihapus@gmail.com>
2018-11-22 21:50:52 +0000

    Update cb.js

--- cb.js
+++ cb.js
@@ -82,7 +82,7 @@ console.log("sort files called....");
 
 function showFiles(allsubmissions) {
   
-  console.log("showfiles called");
+  console.log("showfiles called...");
 
   var myWindow = window.open("", "MsgWindow", "width=200,height=100");
 myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"); 
