844775d99d90a14a3601a325e35b2fc43146a96d
cihapus <cihapus@gmail.com>
2018-11-22 20:15:37 +0000

    Update cb.js

--- cb.js
+++ cb.js
@@ -4,6 +4,14 @@ function sortFiles(){
 
 console.log("sort files called....");
 
+  var studentfiles = document.getElementById("report-results");
+  
+  if(studentfiles){
+  console.log(studentfiles);
+  }
+  else{
+  console.log("failed to find report results");
+  }
 
 
 }
