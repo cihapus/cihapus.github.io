3bdf64bb652f9b935a037a3a1a0fa2ec1929f1f3
cihapus <cihapus@gmail.com>
2018-11-22 21:29:06 +0000

    Update cb.js

--- cb.js
+++ cb.js
@@ -1,6 +1,13 @@
 
 
 function sortFiles(){
+  
+  var allsubmissions =[];
+  
+  allsubmissions[0]=[];
+  allsubmissions[1]=[];
+  allsubmissions[2]=[];
+  
 
 console.log("sort files called....");
 
@@ -30,8 +37,25 @@ console.log("sort files called....");
         
         var critiquenumber = splitsubmission[0].textContent;
         
-        console.log(addressofeach, nameofeach, critiquenumber);
+        //console.log(addressofeach, nameofeach, critiquenumber);
         
+        var thissubmission = {"address": addressofeach, "name": nameofeach};
+        
+        if(critiquenumber=="Critique 1"){
+        allsubmissions[0].push(thissubmission);
+        }
+          else if(critiquenumber=="Critique 2"){
+         allsubmissions[1].push(thissubmission);
+        }
+          else if(critiquenumber=="Critique 3"){
+         allsubmissions[2].push(thissubmission);
+        }
+        
+        //works -- gives number of files and breaks them up into clickable links with student name and critique number
+        // if critiquenumber ==="Critique 1" then push to that array, if "Critique 2" push to that array etc
+        //that should give three lists of files which can be downloaded to correct folders manually
+        
+        console.log("allsubmissions== "+allsubmissions);
         
         
       
