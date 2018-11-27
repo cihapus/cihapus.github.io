14219ac425698a0dcb4002dd96c9503a118a34ed
cihapus <cihapus@gmail.com>
2018-11-27 15:27:24 +0000

    Update cb.js

--- cb.js
+++ cb.js
@@ -5,7 +5,7 @@ function sortFiles(){
   
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = "#submissions a:active { color: red, border:1px solid #000 }";
+  css.innerHTML = "#submissions a:active { border: 1px solid red }";
   document.head.appendChild(css);
   
   
