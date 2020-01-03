5973a251d868cd9a8f50848905cdd93eceb2da7f
cihapus <cihapus@gmail.com>
2020-01-03 22:09:11 +0000

    Update hwg.js

--- hwg.js
+++ hwg.js
@@ -1,7 +1,7 @@
 function initMeta(){
 
   console.log("working...");
-alert("hello world");
+//alert("hello world");
   
   if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
@@ -13,7 +13,65 @@ alert("hello world");
   
   
   
+  var messagewindow = document.createElement("DIV");
+  messagewindow.id = "submissions";
+  
+   var messagewindowbutton = document.createElement("BUTTON");
+  messagewindowbutton.id = "closemessagewindow";
+  messagewindowbutton.innerHTML = "close popup";
+  
+  messagewindow.style.zIndex = "9999";
+  
+   messagewindow.style.minHeight="400px";
+  messagewindow.style.minWidth="1000px";
+  messagewindow.style.maxHeight="600px";
+  
+  
+  messagewindow.style.position = "fixed";
+messagewindow.style.top = "20px";
+messagewindow.style.left = "0px";
+messagewindow.style.margin = "20px";
+  messagewindow.style.padding = "20px";
+  
+messagewindow.style.backgroundColor = "white"; 
+  messagewindow.style.color="black";
+  messagewindow.style.overflowY = "scroll";
+  
+  
+  
+  var title = document.createElement("H2")               
+  var titleContent = document.createTextNode("Critique Submissions for ");
+  title.appendChild(titleContent);
+  
+  messagewindow.appendChild(title);  
 
+ messagewindow.appendChild(messagewindowbutton);  
+  
+ 
+  var getbody = document.querySelector("body"); 
+  document.body.appendChild(messagewindow); 
+  
+  
+  var addclosehandler = document.getElementById("closemessagewindow");
+  addclosehandler.addEventListener('click', closeWindow);
+
+function closeWindow(){
+
+console.log("close window");
+  
+  var subwindow = document.getElementById("submissions");
+   subwindow.parentNode.removeChild(subwindow);
+  
+  
 }
+
+
+
+  
+  
+  
+  
+
+}//initMeta
 
 initMeta();
