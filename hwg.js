6ee3d64163d80ad630dbca3a2f7d4adeea46a8d6
cihapus <cihapus@gmail.com>
2020-01-03 21:57:13 +0000

    Update hwg.js

--- hwg.js
+++ hwg.js
@@ -2,6 +2,17 @@ function initMeta(){
 
   console.log("working...");
 alert("hello world");
+  
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+    console.log("file reader support");
+} else {
+  console.warn('The File APIs are not fully supported in this browser.');
+}
+  
+  
+  
+  
 
 }
 
