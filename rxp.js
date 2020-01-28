1b84f2be90c39b24481d0929a3b3d626a98394ac
cihapus <cihapus@gmail.com>
2020-01-28 21:40:14 +0000

    Update rxp.js

--- rxp.js
+++ rxp.js
@@ -50,7 +50,40 @@ test.id = "testing";
 test.innerHTML="<h1>hello world</h1>";
 document.body.appendChild(test);
 
-ReactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));    
+    
+    
+    
+    const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+    
+    
+    
+    const domContainer = document.getElementById("testing");
+ReactDOM.render(e(LikeButton), domContainer);
+    
+    
+    
+    
+    
+//ReactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));    
     
 }
 
