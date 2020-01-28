12d73a41deb92b34e7faccc052eb664c6eb72472
cihapus <cihapus@gmail.com>
2020-01-28 20:41:14 +0000

    Update rxp.js

--- rxp.js
+++ rxp.js
@@ -1,15 +1,45 @@
-import * as React from './react.js'
+//import * as React from './react.js'
 //import * as React from "https://unpkg.com/react@16/umd/react.development.js";
 //import * as React from "https://unpkg.com/react@16.12.0/umd/react.production.min.js";
 
-console.log("v1");
+
+
+    var head=document.getElementsByTagName("head")[0];
+    var script=document.createElement('script');
+    script.src="https://unpkg.com/react@16/umd/react.development.js";
+    script.type='text/javascript';
+    //real browsers
+    script.onload=trythis();
+    //Internet explorer
+    script.onreadystatechange = function() {
+        if (this.readyState == 'complete') {
+            trythis();
+        }
+    }
+    head.appendChild(script);
+
+
+
+function trythis(){
+
+  console.log("scripts loaded");
+
+}
+
+//console.log("v1");
 
 
 //import 'ReactDOM' from './react-dom.js'
 
-var test = document.createElement('div');
-test.id = "testing";
-test.innerHTML = "<h1>Hello world</h1>";
+//var test = document.createElement('div');
+//test.id = "testing";
+//test.innerHTML="<h1>hello world</h1>";
+//document.body.appendChild(test);
 
-document.body.appendChild(test);
+
+
+//test.innerHTML = `<script src='https://unpkg.com/react@16/umd/react.development.js'></script>
+//<script src='https://unpkg.com/react@16.12.0/umd/react.production.min.js'></script>`;
+
+//document.body.appendChild(test);
 //reactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));
