5d4b6cffeeec305563b529526a12cd554055507e
cihapus <cihapus@gmail.com>
2020-01-28 20:08:03 +0000

    Update rxp.js

--- rxp.js
+++ rxp.js
@@ -1,5 +1,5 @@
 //import 'React' from './react.js'
-import React as module from "https://unpkg.com/react@16/umd/react.development.js";
+import 'React' as module from "https://unpkg.com/react@16/umd/react.development.js";
 
 console.log("v1");
 
