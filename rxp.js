57d16328d8dee0c2c22ec953d17abab2d20cd72b
cihapus <cihapus@gmail.com>
2020-01-28 19:43:25 +0000

    Create rxp.js

---
+++ rxp.js
@@ -0,0 +1,10 @@
+import 'React' from './react.js'
+import 'ReactDOM' from './react-dom.js'
+
+var test = document.createElement('div');
+test.id = "testing";
+test.innerHTML = "<h1>Hello world</h1>";
+var attachto = document.body;
+attachto.appendChild(test);
+
+reactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));
