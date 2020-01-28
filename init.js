6555a6068e270c9c273df4ad46f18cb28ebe2dac
cihapus <cihapus@gmail.com>
2020-01-28 20:59:34 +0000

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,13 @@
 function init(){
 
 console.log("logging from init");
+  
+  var test = document.createElement("div");
+  test.id="testing";
+  test.innerHTML="<h1>hello world</h1>";
+  
+  ReactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));
+  
 
 }
 
