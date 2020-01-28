e3423f548841920ae0692517211b3fb265ce149a
cihapus <cihapus@gmail.com>
2020-01-28 21:00:13 +0000

    Update init.js

--- init.js
+++ init.js
@@ -5,6 +5,7 @@ console.log("logging from init");
   var test = document.createElement("div");
   test.id="testing";
   test.innerHTML="<h1>hello world</h1>";
+  document.body.appendChild(test);
   
   ReactDOM.render(<h2>hello from react</h2>, document.getElementById('testing'));
   
