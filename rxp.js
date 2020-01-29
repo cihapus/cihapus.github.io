0ddbac7ffd7d23b90f8ae5d8c455d4be8190fb15
cihapus <cihapus@gmail.com>
2020-01-29 15:33:19 +0000

    Update rxp.js

--- rxp.js
+++ rxp.js
@@ -1,10 +1,8 @@
-//import * as React from './react.js'
-//import * as React from "https://unpkg.com/react@16/umd/react.development.js";
-//import * as React from "https://unpkg.com/react@16.12.0/umd/react.production.min.js";
+//loads necessary dependencies
 
 import {init} from './init.js';
 
-
+//load external scripts in sequence
 var scripts=["https://unpkg.com/react@16/umd/react.development.js",
 "https://unpkg.com/react-dom@16/umd/react-dom.development.js",
     "https://unpkg.com/babel-standalone@6/babel.min.js"];
@@ -12,93 +10,110 @@ var scripts=["https://unpkg.com/react@16/umd/react.development.js",
 var head=document.getElementsByTagName("head")[0];
 
 var scriptElements=[];
+var scriptCount = 0;
+var totalScripts = scripts.length;
+
+for(let i=0;i<scripts.length;i++){
+
+		var script = document.createElement('script');
+		  script.src = src;
+		  script.async = false;
+		  script.type='text/javascript';
+	
+	
+	
+	if(i!=scripts.length-1){
+		 scriptElements[i].addEventListener('load', function(){
+		  head.appendChild(scriptElements[i+1]);
+		 });
+		
+	}
+	else{
+	scriptElements[i].addEventListener('load', function(){
+		 init();
+	});
+	}
+	 head.appendChild(scriptElements[i]);
+
+}
+
+
+/*
 scripts.forEach(function(src) {
 		  var script = document.createElement('script');
 		  script.src = src;
 		  script.async = false;
 		  script.type='text/javascript';
-          scriptElements.push(script);
+         	
+	
+		scriptElements.push(script);
 		});
 
-
+//callback loop to load scripts
 scriptElements[0].addEventListener('load', function(){
-   console.log("script 0 loaded");
+ 
     head.appendChild(scriptElements[1]);
-     scriptElements[1].addEventListener('load', function(){
-         console.log("script 1 loaded");
-     head.appendChild(scriptElements[2]);
-          scriptElements[2].addEventListener('load', function(){
-              console.log("script 2 loaded");
+	
+    	 scriptElements[1].addEventListener('load', function(){
+         
+    		 head.appendChild(scriptElements[2]);
+		 
+          		scriptElements[2].addEventListener('load', function(){
               
-              console.log("react is "+window.React, window.ReactDOM);
-    
-    var test = document.createElement('div');
-test.id = "testing";
-test.innerHTML="<h1>hello world</h1>";
-document.body.appendChild(test);
+    			//test - move to init
+    			var test = document.createElement('div');
+			test.id = "testing";
+			test.innerHTML="<h1>hello world</h1>";
+			document.body.appendChild(test);
               
-              
-              
-              init();
-          });//element2
+              		init();
+          
+		});//element2
      });//element1
 });//element0
 
-
+//fires the script loading loop
 head.appendChild(scriptElements[0]);
+*/
 
-
+		
+		
+		
+		
+		
 
 /*
     var head=document.getElementsByTagName("head")[0];
     var script=document.createElement('script');
     script.src="https://unpkg.com/react@16/umd/react.development.js";
     script.type='text/javascript';
-   
 script.async =false;
-
 script.addEventListener("load", function(){
     console.log("script1 loaded");
     trythis();
 });
-
-
-   
     head.appendChild(script);
 
-
-
 function trythis(){
-
   console.log("first script loaded");
-    
       var head=document.getElementsByTagName("head")[0];
     var script2=document.createElement('script');
     script2.src="https://unpkg.com/react-dom@16/umd/react-dom.development.js";
     script2.type='text/javascript';
     script2.async =false;
-    
     script2.addEventListener("load", function(){
         console.log("script2 loaded");
     trythistoo();
     });
-    
      head.appendChild(script2);
-
-
 }
 
 function trythistoo(){
-
 console.log("try this too - react is "+window.React, window.ReactDOM);
-    
     var test = document.createElement('div');
 test.id = "testing";
 test.innerHTML="<h1>hello world</h1>";
 document.body.appendChild(test);
-
-    
     init();  
-    
 }
 */
