3564ddc247a456b401772c6970767a1bf9b190b2
cihapus <cihapus@gmail.com>
2020-01-30 20:58:15 +0000

    Update init.js

--- init.js
+++ init.js
@@ -8,13 +8,7 @@ import { saveAs } from './filesaver.js';
 function init(){
 
 console.log("logging from init");
-  //console.log("react here is "+window.React);
-  
-  //const React = window.React;
-  //const ReactDOM = window.ReactDOM;
-  
-  //console.log(React, ReactDOM);
-  
+ 
   var test = document.createElement("div");
   test.id="testinit";
   test.innerHTML="<h1>hello world from init</h1>";
@@ -34,11 +28,6 @@ class LikeButton extends React.Component {
     if (this.state.liked) {
       return 'You liked this.';
     }
-
-    
-   
-    
-    
     
     return e(
       'button',
