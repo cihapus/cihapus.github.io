21a092e39e24bef7675a438c7cebd7ca9673c7f8
cihapus <cihapus@gmail.com>
2020-01-30 14:34:23 +0000

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 //this is called by rxp when react and reactdom have loaded
 //need babel.js as well for jsx
 
+import {Compressor} from './compressor.js';
+
+
+
 function init(){
 
 console.log("logging from init");
