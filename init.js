b71fac3210209d8a400a7905131c2387660b661b
cihapus <cihapus@gmail.com>
2020-02-01 22:35:12 +0000

    Update init.js

--- init.js
+++ init.js
@@ -173,7 +173,6 @@ class SubmitForm extends React.Component {
     borderRadius: "4px"
 };
    
-   const closeBtnContent = "X";
    
    
    
@@ -193,7 +192,7 @@ class SubmitForm extends React.Component {
         style: closeBtnStyle,
         onClick: () => this.setState({ clicked: true }) 
        },
-     {closeBtnContent}
+     'X'
     );
   }
 }
